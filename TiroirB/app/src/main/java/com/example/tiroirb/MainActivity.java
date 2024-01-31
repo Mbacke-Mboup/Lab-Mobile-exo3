c8ea6b6d7611cef0197877dca3c1300200c85425
m <2274252@cegepmontpetit.ca>
2024-01-31 14:39:46 -0500

    FIn

--- TiroirB/app/src/main/java/com/example/tiroirb/MainActivity.java
+++ TiroirB/app/src/main/java/com/example/tiroirb/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.tiroirb;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.tiroirb.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+
+    ActionBarDrawerToggle abToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,11 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         NavigationView nv = binding.navigationView;
+        DrawerLayout dl = binding.drawerLayout;
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        abToggle = new ActionBarDrawerToggle(this, dl,R.string.draw_open, R.string.draw_close);
+        dl.addDrawerListener(abToggle);
+        abToggle.syncState();
         nv.setNavigationItemSelectedListener(item -> {
             if (item.getItemId() == R.id.accueil) {
                 this.finish();
@@ -28,5 +41,11 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(abToggle.onOptionsItemSelected(item)) return true;
+        return super.onOptionsItemSelected(item);
     }
 }
